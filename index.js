88bd3b9707a959d73e91e020446675a15bdb8835
Matthew Baty <mattbaty@outlook.com>
2023-03-22 20:24:14 -0400

    started questions

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+//packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateREADME = require('./utils/generateMarkdown.js');
+
+// questions for README
+const questions = [
+    {
+        type: 'input',
+        message: ""
+    }
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
